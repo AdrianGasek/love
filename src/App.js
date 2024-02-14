0ef713d7a22d8b0e3f35e6829facbd6aa2643e38
adrian.gasek <adrian.gasek.99@gmail.com>
2024-02-14 12:33:30 +0100

    add full created page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+import heartGif from './assets/heart.gif';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div className="love-container">
+      <div className="love-content">
+        <h2 className="love-title">Kocham Cię!</h2>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Żanetko K. jesteś dla mnie wszystkim!<br/>
+          Nie mogę bez Ciebie żyć!<br/>
+          Każdy poranek jest dla mnie radosny bo wiem że mam Ciebie!<br/>
+          Moje serce przepełnia miłość do Ciebie!<br />
+          Niech ta strona istnieje w internecie w nieskończoność,<br />
+          niech będzie wiecznym dowodem mojej miłości!
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <img className="love-author-heart" src={heartGif} alt="Nigdy nie przestanie bić" />
+        <h3 className="love-author">Twój Adrian G.</h3>
+        <h3 className="love-date">14.02.2024</h3>
+      </div>
     </div>
   );
 }
